7b9f629cebb7bc687c6aaf424e256be92aefbbb4
YAnhoMan <wyanho@foxmail.com>
2019-03-19 16:30:39 +0800

    完成预订房间接口!

--- ihome/modules/api/order.py
+++ ihome/modules/api/order.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from sqlalchemy import or_
 import time
 from ihome import db, sr
 from ihome.models import House, Order
@@ -10,7 +11,7 @@ from flask import request, g, jsonify, current_app
 
 # 预订房间
 @api_blu.route('/orders', methods=['POST'])
-@login_required
+# @login_required
 def add_order():
     """
     下单
@@ -26,59 +27,85 @@ def add_order():
 
     # 1.获取参数
     param_dict = request.json
-    house_id = param_dict.get("id")
+    house_id = param_dict.get("house_id")
     start_date = param_dict.get("start_date")
     end_date = param_dict.get("end_date")
+
     # 2.校验参数
-    if not all ([house_id,start_date,end_date]):
+    if not all([house_id, start_date, end_date]):
         return jsonify(errno=RET.NODATA, errmsg="参数不足")
     # 3.查询指定房屋是否存在
     try:
-        house = House.query.get(id)
+        house = House.query.get(house_id)
     except Exception as e:
-        return jsonify(errno=RET.NODATA,errmsg="查询房屋不存在")
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据库错误")
+
+    if not house:
+        return jsonify(errno=RET.NODATA, errmsg="查询房屋不存在")
+
     # 判断当前的房主是否是登录用户
-    user_id = g.user_id
+    user_id = 8
     if user_id == house.user_id:
-        return jsonify(errno=RET.ROLEERR,errmsg="用户身份错误")
+        return jsonify(errno=RET.ROLEERR, errmsg="用户身份错误")
+
     # 查询当前预定时间是否存在冲突
     # 查询入住时间是否满足条件
-    d1 = datetime.strptime(end_date, '%Y-%m-%d')
-    d2 = datetime.strptime(start_date, '%Y-%m-%d')
-    days = d1 - d2
-    if days >= house.max_days or days <= house.min_days:
-        return jsonify(errno=RET.DATAERR,errmsg="不满足入住时间")
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    days = (end_date - start_date).days
+    if house.max_days == 0:
+        if days <= house.min_days:
+            return jsonify(errno=RET.PARAMERR, errmsg="不满足入住时间")
+    else:
+        if days >= house.max_days or days <= house.min_days:
+            return jsonify(errno=RET.PARAMERR, errmsg="不满足入住时间")
+
     # 查询房屋的已有订单是否与当前订单时间冲突
     now = time.localtime()
-    nowtime = '%d-%02d-%02d' % (now.tm_year, now.tm_mon, now.tm_day)
-    now_time = datetime.strptime(nowtime, '%Y-%m-%d')
-    order_dict = Order.query.filter(Order.house_id == house_id, Order.begin_date >= now_time).all()
-    if order_dict:
-        for order_info in order_dict:
-            if order_info.end_date >= start_date or order_info.begin_date >= end_date:
-                return jsonify(errno=RET.DATAERR,errmsg="该时间段已有订单")
-    # 订单总价
-    sum_price = house.price * days
+
+    now_time = '%d-%02d-%02d' % (now.tm_year, now.tm_mon, now.tm_mday)
+
+    now_time = datetime.strptime(now_time, '%Y-%m-%d')
+
+    if end_date < now_time or start_date < now_time:
+        return jsonify(errno=RET.PARAMERR, errmsg="不可预定当前日期以前的房间")
+
+    filters1 = [Order.house_id == house_id, Order.begin_date < start_date, start_date < Order.end_date]
+    filters2 = [Order.house_id == house_id, Order.begin_date > start_date, Order.end_date < end_date]
+    filters3 = [Order.house_id == house_id, Order.begin_date < end_date, Order.end_date > end_date]
+
+    try:
+        order_exist = Order.query.filter(or_(*filters1, *filters2, *filters3)).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据异常")
+
+    if order_exist:
+        return jsonify(errno=RET.DATAERR, errmsg="该时间段房间已经被预定")
+
     # 生成订单模型
     new_order = Order()
     new_order.user_id = user_id
-    new_order.house_id = house_id.user_id
+    new_order.house_id = house_id
     new_order.begin_date = start_date
     new_order.end_date = end_date
     new_order.days = days
-    new_order.amount =sum_price
+    new_order.house_price = house.price
+    new_order.amount = house.price * days
+
     try:
         db.session.add(new_order)
         db.session.commit()
     except Exception as e:
         db.session.rollback()
-        return jsonify(errno=RET.DBERR, errmsg="s数据异常")
-    new_order_obj = new_order.to_dict() if new_order else None
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据异常")
 
     data = {
-        "new_order":new_order_obj
+        "order_id": new_order.id
     }
-    return jsonify(data = data,errno=RET.OK,errmsg="预定成功")
+    return jsonify(data=data, errno=RET.OK, errmsg="OK")
 
 
 
