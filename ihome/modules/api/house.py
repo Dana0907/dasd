c719c6d15fdf0cbc1dfb4327249212a17d8c771c
wxwcq <2298979312@qq.com>
2019-03-20 11:28:02 +0800

    新房源发布&图片上传

--- ihome/modules/api/house.py
+++ ihome/modules/api/house.py
@@ -22,7 +22,7 @@ def get_user_house_list():
     2. 查询数据
     :return:
     """
-   pass
+    pass
 
 
 # 获取地区信息
@@ -35,6 +35,7 @@ def get_areas():
     """
     pass
 
+
 # 上传房屋图片
 @api_blu.route("/houses/<int:house_id>/images", methods=['POST'])
 @login_required
@@ -46,7 +47,43 @@ def upload_house_image(house_id):
     4. 进行返回
     :return:
     """
-    pass
+    # 获取参数
+    house_image = request.files.get('images')
+    house_id = house_id
+    user = g.ih_user_profile
+
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 将图片上传到七牛云
+    try:
+        image_name = storage_image(house_image.read())
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="上传图片到七牛云异常")
+        # 图片名称没有值
+    if not image_name:
+        return jsonify(errno=RET.DBERR, errmsg="上传图片到七牛云异常")
+
+    image = HouseImage()
+    image.url = constants.QINIU_DOMIN_PREFIX + image_name
+    image.house_id = house_id
+
+    data = {
+        "url": image.url,
+
+    }
+
+    try:
+        db.session.add(image)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        # 数据库回滚
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errormsg="保存数据对象异常")
+
+    return jsonify(error=RET.OK, errmsg="上传图片成功", data=data)
 
 
 # 发布房源
@@ -75,7 +112,60 @@ def save_new_house():
     }
     :return:
     """
-    pass
+    # POST请求,获取参数
+
+    title = request.form.get('title')
+    price = request.form.get('price')
+    area_id = request.form.get('area_id')
+    address = request.form.get('address')
+    room_count = request.form.get('room_count')
+    acreage = request.form.get('acreage')
+    unit = request.form.get('unit')
+    capacity = request.form.get('capacity')
+    beds = request.form.get('beds')
+    deposit = request.form.get('deposit')
+    min_days = request.form.get('min_days')
+    max_days = request.form.get('max_days')
+    facility = request.form.get('facility')
+    user = g.ih_user_profile
+    # 非空判断
+    if not all([title, price, area_id, address, room_count, acreage, unit, capacity, beds, deposit, min_days, max_days,
+                facility]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    house = House()
+    # 标题
+    house.title = str(title)
+    # 价格
+    house.price = str(price)
+    house.area_id = int(area_id)
+    house.address = str(address)
+    house.room_count = int(room_count)
+    house.acreage = int(acreage)
+    house.unit = str(unit)
+    house.capacity = int(capacity)
+    house.beds = str(beds)
+    house.deposit = str(deposit)
+    house.min_days = int(min_days)
+    house.max_days = int(max_days)
+    house.facility = facility
+    house.user_id = g.ih_user_profile.id
+    house_id = g.ih_house_info.id
+
+    # 3.2 保存回数据库
+    try:
+        db.session.add(house)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        # 数据库回滚
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errormsg="保存房源对象异常")
+
+    return jsonify(error=RET.OK, errmsg="发布房源成功", data={house_id})
 
 
 # 房屋详情
